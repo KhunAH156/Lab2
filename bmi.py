880a68793fea1bdd88022e228dd403a3998b48be
KhunAungHein <khunah.23@ichat.sp.edu.sg>
2024-10-28 09:08:04 +0800

    added return

--- bmi.py
+++ bmi.py
@@ -8,9 +8,13 @@ def calculate_bmi(height, weight):
 
     if BMI < 18.5:
         print("Under Weight")
+        return -1
     elif BMI >= 18.5 and BMI <= 25.0:
         print("Normal Weight")
-    else: print("Over Weight")    
+        return 0
+    else: 
+        print("Over Weight") 
+        return 1   
 
 
 calculate_bmi(weight=57,height=1.73)
